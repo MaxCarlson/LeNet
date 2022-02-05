e349ec5f2602aac54959bbc649719c8478b36550
Maxwell Carlson <bart4282819@gmail.com>
2022-02-04 21:08:55 -0800

    Add project files.

---
+++ LeNet/LeNet.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor()])#,
+     #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batchSize = 32
+
+trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+net = nn.Sequential(
+    nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Conv2d(6, 16, kernel_size=5), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Flatten(),
+    nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
+    nn.Linear(120, 84), nn.Sigmoid(),
+    nn.Linear(84, 10)
+    )
